f1e58bb2d2b81f19b29b9d52252c4cd1eddba360
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-08-27 19:09:15 +0530

    Simple String Added

--- 12. Struct/simple.c
+++ 12. Struct/simple.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 struct student {
+  char name[100];
   int height;
   double width;
 };
 
 int main() {
-  struct student student1 = { 6, 10.1 };
-  struct student student2 = { 7, 13.4 };
-  printf("Height of the First Student is: %d And Width is: %0.2f\n", student1.height, student1.width);
-  printf("Height of the Second Student is: %d And Width is: %0.2f\n", student2.height, student2.width);
+  struct student student1 = { "Zawad Ahmed", 6, 10.1 };
+  struct student student2 = { "Maryam Ahmed", 7, 13.4 };
+  printf("Height of the %s is: %d And Width is: %0.2f\n", student1.name, student1.height, student1.width);
+  printf("Height of the %s is: %d And Width is: %0.2f\n", student2.name, student2.height, student2.width);
   return 0;
 }
