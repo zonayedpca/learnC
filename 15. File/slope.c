b8589506801d20ad8c2048cb1c96585a0108ca02
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-02 17:10:05 +0530

    Read and Write Data on and From a File

--- 15. File/slope.c
+++ 15. File/slope.c
@@ -2,29 +2,19 @@
 #define HEIGHT 50
 
 void printbar(int, FILE*);
+void writeData(FILE*);
+void showData(FILE*);
 
 int main() {
-  FILE *file = fopen("slope.txt", "w");
+  FILE *file = fopen("slope.txt", "w+");
   if(!file) {
     printf("Something went wrong! Please, Try Again...\n");
   } else {
-    char name[100];
-    printf("Enter your name: ");
-    gets(name);
-
-    printbar(30, file);
-    fprintf(file, "Welcome to the slope of the uinverse\n");
-    fprintf(file, "Name: %s\n", name);
-    fprintf(file, "Height: %d \n", HEIGHT);
-    printbar(30, file);
-
-    for(int i = 1; i < HEIGHT; ++i) {
-      for(int j = 0; j < i; ++j) {
-        putc('*', file);
-      }
-      putc('\n', file);
-    }
+    writeData(file);
+    printf("Writen data successfully! Now showing the data...\n");
+    showData(file);
   }
+  fclose(file);
   return 0;
 }
 
@@ -33,4 +23,33 @@ void printbar(int length, FILE *file) {
     putc('=', file);
   }
   putc('\n', file);
+}
+
+void writeData(FILE *file) {
+  char name[100];
+  printf("Enter your name: ");
+  gets(name);
+
+  printbar(30, file);
+  fprintf(file, "Welcome to the slope of the uinverse\n");
+  fprintf(file, "Name: %s\n", name);
+  fprintf(file, "Height: %d \n", HEIGHT);
+  printbar(30, file);
+
+  for(int i = 1; i < HEIGHT; ++i) {
+    for(int j = 0; j < i; ++j) {
+      putc('*', file);
+    }
+    putc('\n', file);
+  }
+}
+
+void showData(FILE *file) {
+  char c;
+  rewind(file);
+  while(!feof(file)) {
+    c = getc(file);
+    printf("%c", c);
+  }
+  printf("\n");
 }
