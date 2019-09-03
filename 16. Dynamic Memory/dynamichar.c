7e3908f85ff60e7b0fda3a10962a8f07f9ecc53b
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-03 16:13:24 +0530

    usage of malloc

---
+++ 16. Dynamic Memory/dynamichar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char *name;
+  name = malloc(sizeof(name) * 20);
+  if(name) {
+    printf("Enter your name: ");
+    gets(name);
+    printf("Your name is: %s \n", name);
+    free(name);
+  } else {
+    printf("Something went wrong...\n");
+  }
+  return 0;
+}
